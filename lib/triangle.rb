5c4dbefa917aa95fc6555d34a5f43261dd58ec97
Amit Singh <amit_nyc@live.com>
2018-07-19 16:19:16 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,17 +1,36 @@
 class Triangle
-
-
-attr_accessor :side_1, :side_2, :side_3
-
-
-  def  initialize(side_1:, side_2:, side_3:)
-    @side_1 = side_1
-    @side_2 = side_2
-    @side_3 = side_3
+  attr_accessor :x, :y, :z
+  def initialize(x:, y:, z:)
+    @x = x
+    @y = y
+    @z = z
   end
 
-  def kind(symbol)
-    symbol =
+  def kind
+    if kosher_triangle == true && kosher_triangle2 == true
+      puts true
+    else
+      raise TriangleError
+    end
 
+    if x == y && y == z && x == z
+      :equilateral
+    elsif x != y && y != z && z != x
+      :scalene
+    else
+      :isosceles
+    end
   end
+
+  def kosher_triangle
+    x.positive? && y.positive? && z.positive?
+  end
+
+  def kosher_triangle2
+    x + y > z && x + z > y && y + z > x
+  end
+end
+
+class TriangleError < StandardError
+
 end
